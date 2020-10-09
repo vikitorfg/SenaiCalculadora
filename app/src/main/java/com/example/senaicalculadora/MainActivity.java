3788b8fb1e7a0f81070849269b5d9e215a5bedc4
Victor Garzel <victorgarzel@hotmail.com>
2020-10-09 16:12:43 -0300

    initial commit

---
+++ app/src/main/java/com/example/senaicalculadora/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.senaicalculadora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
